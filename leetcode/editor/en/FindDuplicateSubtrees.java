b69c15b428baffa595d64bf42c43c04238467387
dive2Pro <toylucklebo@gmail.com>
2020-04-16 11:43:30 +0800

    652 找到有相同结构的子树 (13ms, 87.45%) 猜测: 在递归到右节点的时候 new StringBuilder()了       多了个 equals("") 的判断

---
+++ leetcode/editor/en/FindDuplicateSubtrees.java
@@ -0,0 +1,100 @@
+package leetcode.editor.en;
+
+//Given a binary tree, return all duplicate subtrees. For each kind of duplicate
+// subtrees, you only need to return the root node of any one of them. 
+//
+// Two trees are duplicate if they have the same structure with same node values
+//. 
+//
+// Example 1: 
+//
+// 
+//        1
+//       / \
+//      2   3
+//     /   / \
+//    4   2   4
+//       /
+//      4
+// 
+//
+// The following are two duplicate subtrees: 
+//
+// 
+//      2
+//     /
+//    4
+// 
+//
+// and 
+//
+// 
+//    4
+// 
+//Therefore, you need to return above trees' root in the form of a list. Related
+// Topics Tree
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FindDuplicateSubtrees {
+    public static void main(String[] args) {
+        Solution solution = new FindDuplicateSubtrees().new Solution();
+        solution.findDuplicateSubtrees(TreeNode.generateFrom(new Object[]{
+                1, 2, 3, 4, null, 2, 4, null, null, 4
+//                0, 0, 0, 0, null, null, 0, null, null, null, 0
+        }));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+            ArrayList<TreeNode> total = new ArrayList<>();
+            if (root == null) {
+                return total;
+            }
+            HashMap<String, Integer> map = new HashMap<>();
+            dfs(root.left, total, new StringBuilder(), map);
+            dfs(root.right, total, new StringBuilder(), map);
+            return total;
+        }
+
+        private String dfs(TreeNode root, ArrayList<TreeNode> total, StringBuilder stringBuilder, HashMap<String, Integer> map) {
+            if (root == null) {
+                return "X";
+            }
+            String left = dfs(root.left, total, stringBuilder, map);
+            stringBuilder.append(left);
+            stringBuilder.insert(0, root.val);
+            String right = dfs(root.right, total, new StringBuilder(), map);
+            stringBuilder.append(right);
+
+            String result = stringBuilder.toString();
+
+            if (!result.equals("")) {
+                int val = map.getOrDefault(result, 0) + 1;
+                map.put(result, val);
+                if (val == 2) {
+                    total.add(0, root);
+                }
+            }
+            stringBuilder.setLength(0);
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
