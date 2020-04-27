3f5131a0145b0dfdd3e50d71505bf42ac4d3ff4a
dive2Pro <toylucklebo@gmail.com>
2020-04-27 09:34:17 +0800

    392 IsSubsequence (5ms, 81.69%)

---
+++ leetcode/editor/en/IsSubsequence.java
@@ -0,0 +1,72 @@
+package leetcode.editor.en;
+
+//
+//Given a string s and a string t, check if s is subsequence of t.
+// 
+//
+// 
+//You may assume that there is only lower case English letters in both s and t. 
+//t is potentially a very long (length ~= 500,000) string, and s is a short string
+// (<=100).
+// 
+//
+// 
+//A subsequence of a string is a new string which is formed from the original st
+//ring by deleting some (can be none) of the characters without disturbing the rel
+//ative positions of the remaining characters. (ie, "ace" is a subsequence of "abc
+//de" while "aec" is not).
+// 
+//
+// Example 1: 
+//s = "abc", t = "ahbgdc"
+// 
+// 
+//Return true.
+// 
+//
+// Example 2: 
+//s = "axc", t = "ahbgdc"
+// 
+// 
+//Return false.
+// 
+//
+// Follow up: 
+//If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you w
+//ant to check one by one to see if T has its subsequence. In this scenario, how w
+//ould you change your code? 
+//
+// Credits: Special thanks to @pbrother for adding this problem and creating all
+// test cases. Related Topics Binary Search Dynamic Programming Greedy
+
+
+public class IsSubsequence {
+    public static void main(String[] args) {
+        Solution solution = new IsSubsequence().new Solution();
+        solution.isSubsequence("abc", "ahbgdc");
+        solution.isSubsequence("ccc", "ahbgdc");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+            int j = 0;
+            int i = 0;
+            boolean runOut = s.equals("");
+            for (; i < s.length(); i++) {
+                char c = s.charAt(i);
+                for (; j < t.length(); ) {
+                    if (c == t.charAt(j++)) {
+                        runOut = i == s.length() - 1;
+                        break;
+                    }
+                }
+            }
+
+            return runOut;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
