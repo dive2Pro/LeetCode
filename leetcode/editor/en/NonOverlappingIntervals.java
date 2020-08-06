c240e1dcdf9bc0f318e2788a3bd53f0dd4835a82
hyc <toylucklebo@outlook.com>
2020-08-06 16:42:54 +0800

    🔃 435 Non Overlapping Intervals

--- leetcode/editor/en/NonOverlappingIntervals.java
+++ leetcode/editor/en/NonOverlappingIntervals.java
@@ -13,8 +13,7 @@ package leetcode.editor.en;
 // 
 //Input: [[1,2],[2,3],[3,4],[1,3]]
 //Output: 1
-//Explanation: [1,3] can be removed and the rest of intervals are non-overlappin
-//g.
+//Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
 // 
 //
 // Example 2: 
@@ -22,8 +21,7 @@ package leetcode.editor.en;
 // 
 //Input: [[1,2],[1,2],[1,2]]
 //Output: 2
-//Explanation: You need to remove two [1,2] to make the rest of intervals non-ov
-//erlapping.
+//Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
 // 
 //
 // Example 3: 
@@ -31,8 +29,8 @@ package leetcode.editor.en;
 // 
 //Input: [[1,2],[2,3]]
 //Output: 0
-//Explanation: You don't need to remove any of the intervals since they're alrea
-//dy non-overlapping.
+//Explanation: You don't need to remove any of the intervals since they're
+// already non-overlapping.
 // 
 //
 // 
@@ -90,6 +88,28 @@ public class NonOverlappingIntervals {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        // [[August 5, 2020]]
+        public int eraseOverlapIntervals(int[][] intervals) {
+            Arrays.sort(intervals, (p, q) -> {
+                return p[1] - q[1];
+            });
+
+            int end = intervals[0][1];
+            // 记录的是非 overlapping 的个数
+            int count = 1;
+
+            for (int i = 1; i < intervals.length; i++) {
+                // Greedy
+                // 只需要拿到可组合成一条非 overlapping 的 intervals
+                if(intervals[i][0] >= end) {
+                    end = intervals[i][1];
+                    count ++;
+                }
+            }
+            return intervals.length - count;
+        }
+    }
+    class Solution2 {
         public int eraseOverlapIntervals(int[][] intervals) {
             if (intervals.length == 0) return 0;
             Arrays.sort(intervals, (p, q) -> {
